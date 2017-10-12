eb0f3a92e7693ddd477f1dcf675f511544146414
Cooper <sverige.cooper@gmail.com>
2017-10-12 17:58:32 +0200

    added version number

--- html/region.php
+++ html/region.php
@@ -108,7 +108,7 @@ if (isset($_GET["r"])) {
 			?>
 
 			<div id="footer">
-				<p>Developed by <a href="http://b0gosort.github.io">Solborg Development</a></p>
+				<p>Sovereign.Land Alpha 0.1.0, Developed by <a href="http://b0gosort.github.io" target="_blank">Solborg Development</a></p>
 				<p>Copyright &copy; 2017 Cooper Johnston</p>
 			</div>
 		</div>
